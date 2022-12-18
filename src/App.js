3edad4e0c6dfdb99d5008169118caa278baf2a3d
raghav-rama <hackerer528@gmail.com>
2022-12-19 03:17:09 +0530

    hotfix: remove non working code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import InjectWallet from './components/InjectWallet';
-import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
       <InjectWallet/>
     </div>
   );
